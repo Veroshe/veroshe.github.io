6ded37db9e306c8f175d7622f14d3ef141fea3e3
Veroshe <wkalinowska406@gmail.com>
2017-10-19 17:55:12 +0200

    Portfolio update

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+    <div class="pos-f-t" style="background-color:#E8EDF3;">
+        <div class="collapse" id="navbarToggleExternalContent" >
+            <div class="navbar-colapse" id="navbarNav" ">
+                <ul class="navbar-nav " >
+                    <li class="nav-item hidden-nav">
+                        <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item hidden-nav">
+                        <a class="nav-link" href="#">O mnie</a>
+                    </li>
+                    <li class="nav-item hidden-nav">
+                        <a class="nav-link" href="#">Projekty</a>
+                    </li>
+                    <li class="nav-item hidden-nav">
+                        <a class="nav-link" href="#">Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <nav class="navbar bg-faded" style="text-align: -webkit-center; background-color:#E8EDF3;">
+            <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+    </div>
+
+    <div class="container-fluid" >
+        <div class="row home ">
+
+            <div class="col-sm-4 home-page">
+                        W
+            </div>
+            <div class="col-sm-4 home-page">
+                <div id="triangle"> </div>
+            </div>
+
+            <div class="col-sm-4 home-page">
+                        K
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container-fluid " id="about-container" >
+        <div id="about">
+
+            <h2> Nazywam się Weronika Kalinowska. </h2>
+            <p class="text-left">
+               <b>  Jestem studentką IV roku informatyki stosowanej na Akademii Górniczo-Hutniczej im. Stanisława Staszica.</b> <br/>
+                 Postanowiłam związać swoją przyszłość z programowaniem ponieważ rozwiązywanie problemów przy pomocy 0 i 1 wywołuje we mnie prawdziwy dreszczyk emocji.<br/>
+                 Uwielbiam estetykę, design, sztukę - uświadomiłam sobie że moja praca musi być związana z tworzeniem 'ładnych rzeczy'.<br/>
+               <b>  Jestem aspirującą front-end developerką i web-designerką. </b><br/>
+            </p>
+            <p class="text-left">
+                 Moje umiejętności: <br/>
+                 - HTML5, CSS3, JavaScript <br/>
+                 - Bootstrap, RWD <br/>
+                 - Git <br/>
+                 - Angular2 <br/>
+                 - C++, OOP <br/>
+             </p>
+        </div>
+    </div>
+
+
+    <div class="container-fluid" id="projects">
+        <div class="row portfolio" style="">
+            <div class="col-sm-4 portfolio">
+            <div class="card">
+                <img class="card-img-top" src="images/proj1.png" alt="Card image" ">
+                <div class="card-block">
+                    <h4 class="card-title">Projekt 1</h4>
+                    <p class="card-text"></p>
+                </div>
+            </div>
+            </div>
+            <div class="col-sm-4 portfolio">
+                <div class="card">
+                    <img class="card-img-top" src="images/proj1.png" alt="Card image">
+                    <div class="card-block">
+                        <h4 class="card-title">Projekt 1</h4>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-sm-4 portfolio">
+                <div class="card">
+                    <img class="card-img-top" src="images/proj1.png" alt="Card image">
+                    <div class="card-block">
+                        <h4 class="card-title">Projekt 1</h4>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+
+</script>
+
+</body>
+
+
+</html>
