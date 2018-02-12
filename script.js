a0d4af36c8ab426265cf0f2ea7b3d20d93d9dbeb
Veroshe <wkalinowska406@gmail.com>
2018-02-12 23:28:03 +0100

    Update of portfolio, adding some canvas animation

---
+++ script.js
@@ -0,0 +1,98 @@
+/**
+ * Created by HP on 12.02.2018.
+ */
+
+window.onload = function() {
+    
+    var canvas = document.getElementById('sparkle');
+    var ctx = canvas.getContext("2d");
+    ctx.scale(100,10);
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    var arrayX = [];
+    var arrayY = [];
+    init();
+    canvas.addEventListener('click',function(event) {
+        var x = event.layerX;
+        var y = event.layerY;
+        console.log(x, y);
+        add(x, y);
+        var color = 'rgb(113, 39, 39)';
+        var percent = 0;
+        fill(x, y, color, percent);
+
+    });
+
+
+    function init() {
+        ctx.font = '20px Helvetica';
+
+        setTimeout(draw, 1000);
+    }
+
+
+    function draw() {
+
+        if (arrayX.length !== 0) {
+            color = 'rgb(255,255,255)';
+            for (i = 0; i < arrayX.length; i++) {
+                fill(arrayX[i], arrayY[i],color,0);
+            }
+            arrayX.length = 0;
+            arrayY.length = 0;
+        }
+        setTimeout(draw, 1000);
+
+    }
+
+
+
+
+    function shadeRGBColor(color, percent) {
+        var f = color.split(","), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent,
+            R = parseInt(f[0].slice(4)), G = parseInt(f[1]), B = parseInt(f[2]);
+        return "rgb(" + (Math.round((t - R) * p) + R) + "," + (Math.round((t - G) * p) + G) + "," + (Math.round((t - B) * p) + B) + ")";
+    }
+
+    function blendRGBColors(c0, c1, p) {
+        var f = c0.split(","), t = c1.split(","), R = parseInt(f[0].slice(4)), G = parseInt(f[1]), B = parseInt(f[2]);
+        return "rgb(" + (Math.round((parseInt(t[0].slice(4)) - R) * p) + R) + "," + (Math.round((parseInt(t[1]) - G) * p) + G) + "," + (Math.round((parseInt(t[2]) - B) * p) + B) + ")";
+    }
+
+    function add(x, y) {
+        arrayX.push(x);
+        arrayY.push(y);
+    }
+
+    function fill(x, y, color, percent) {
+        var z = 0.5;
+        for (i = 0; i < 20; i++) {
+
+            ctx.fillStyle = shadeRGBColor(color, percent);
+
+            //krzyz
+            ctx.fillRect(x + i, y, 1, 1);
+            ctx.fillRect(x - i, y, 1, 1);
+            ctx.fillRect(x, y + i, 1, 1);
+            ctx.fillRect(x, y - i, 1, 1);
+            //skos
+            ctx.fillRect(x - i, y + i, 1, 1);
+            ctx.fillRect(x + i, y + i, 1, 1);
+            ctx.fillRect(x + i, y - i, 1, 1);
+            ctx.fillRect(x - i, y - i, 1, 1);
+            //skosny-skos
+            /*ctx.fillRect(x - i, y + i - z, 1, 1);
+            ctx.fillRect(x - i + z, y + i, 1, 1);
+            ctx.fillRect(x + i - z, y + i, 1, 1);
+            ctx.fillRect(x + i, y + i - z, 1, 1);
+
+            ctx.fillRect(x + i, y - i + z, 1, 1);
+            ctx.fillRect(x + i - z, y - i, 1, 1);
+            ctx.fillRect(x - i + z, y - i, 1, 1);
+            ctx.fillRect(x - i, y - i + z, 1, 1);*/
+            z += 0.05;
+            percent += 0.05;
+        }
+    }
+
+}
