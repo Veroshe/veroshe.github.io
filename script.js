a724d0856d636282492ce7b4e1df7c0a2cc7db31
Weronika Kalinowska <wkalinowska406@gmail.com>
2018-02-13 14:01:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ window.onload = function() {
     var canvas = document.getElementById('sparkle');
     var ctx = canvas.getContext("2d");
     ctx.scale(100,10);
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
+    //ctx.canvas.width = window.innerWidth;
+   // ctx.canvas.height = window.innerHeight;
     var arrayX = [];
     var arrayY = [];
     init();
