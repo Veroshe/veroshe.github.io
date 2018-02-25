688218d9017328f78a3d384b9511295321a364cd
Veroshe <wkalinowska406@gmail.com>
2018-02-25 14:37:39 +0100

    Changed functions in script.js.

--- script.js
+++ script.js
@@ -2,32 +2,31 @@
  * Created by HP on 12.02.2018.
  */
 
-window.onload = function() {
-    
+(function() {
     init();
+    
+
 
     function init() {
-        var canvas = document.getElementById('sparkle');
-        var ctx = canvas.getContext("2d");
+        canvas = document.getElementById('sparkle');
+        ctx = canvas.getContext("2d");
         ctx.scale(100,10);
         ctx.canvas.width = document.getElementById('main-container').offsetWidth;
         ctx.canvas.height = document.getElementById('main-container').offsetHeight;
-        var arrayX = [];
-        var arrayY = [];
+        arrayX = [];
+        arrayY = [];
         ctx.font = '20px Helvetica';
-
-        setTimeout(draw, 1000);
         canvas.addEventListener('click', clickListener);
+        setTimeout(draw, 1000);
     }
+
 
     function draw() {
 
         if (arrayX.length !== 0) {
             color = 'rgb(255,255,255)';
             for (i = 0; i < arrayX.length; i++) {
-                console.log(arrayX[i], arrayY[i]);
                 ctx.clearRect((arrayX[i]-40),(arrayY[i]-40),200,200);
-                console.log("weheheh");
             }
             arrayX.length = 0;
             arrayY.length = 0;
@@ -36,15 +35,14 @@ window.onload = function() {
 
     }
 
-    function shadeRGBColor(color, percent) {
-        var f = color.split(","), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent,
-            R = parseInt(f[0].slice(4)), G = parseInt(f[1]), B = parseInt(f[2]);
-        return "rgb(" + (Math.round((t - R) * p) + R) + "," + (Math.round((t - G) * p) + G) + "," + (Math.round((t - B) * p) + B) + ")";
-    }
-
-    function add(x, y) {
-        arrayX.push(x);
-        arrayY.push(y);
+ 
+    function clickListener(event){
+        var x = event.layerX;
+        var y = event.layerY;
+        add(x, y);
+        var color = 'rgb(113, 39, 39)';
+        var percent = 0;
+        fill(x, y, color, percent);
     }
 
     function fill(x, y, color, percent) {
@@ -90,17 +88,18 @@ window.onload = function() {
         
             percent += 0.025;
             z+=0.5;
-            console.log( ctx.fillStyle, z);
         }
     }
-    function clickListener(event){
-        var x = event.layerX;
-        var y = event.layerY;
-        console.log(x, y);
-        add(x, y);
-        var color = 'rgb(113, 39, 39)';
-        var percent = 0;
-        fill(x, y, color, percent);
+    function shadeRGBColor(color, percent) {
+        var f = color.split(","), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent,
+            R = parseInt(f[0].slice(4)), G = parseInt(f[1]), B = parseInt(f[2]);
+        return "rgb(" + (Math.round((t - R) * p) + R) + "," + (Math.round((t - G) * p) + G) + "," + (Math.round((t - B) * p) + B) + ")";
     }
 
-}
+    function add(x, y) {
+        arrayX.push(x);
+        arrayY.push(y);
+    }
+
+
+})();
